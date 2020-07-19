3a8a4db7e4f653e49d7b9c2eb309c5de087efdb2
Kirill Vercetti <office@kyzima-spb.com>
2020-07-19 22:21:41 +0300

    Fix

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ LABEL maintainer="Kirill Vercetti <office@kyzima-spb.com>"
 
 ENV DEBIAN_FRONTEND noninteractive
 
+RUN apt update
+
 RUN apt install -yq --no-install-recommends wget make \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 GITEA_VERSION=$1
 GITEA_URL=https://github.com/go-gitea/gitea/archive/v${GITEA_VERSION}.tar.gz
 
-if wget -q --method=HEAD $GITEA_URL
+if wget -q --no-check-certificate --method=HEAD $GITEA_URL
 then
     echo -n "Download package... "
     wget --no-check-certificate -qO /tmp/gitea.tar.gz $GITEA_URL
@@ -13,14 +13,23 @@ then
 
     pushd /tmp/gitea
         tar -xzf /tmp/gitea.tar.gz -C . --strip-components=1
+
         make frontend
-        mv public /assets
+
+        if [ ! -d /assets ]
+        then
+            mkdir /assets
+        fi
+
+        rm -rf /assets/*
+
+        mv public/* /assets
     popd
 
     exit 0
 fi
 
-echo "No package found for Gitea v${GITEA_VERSION}."
+echo "No package found for Gitea v${GITEA_VERSION}"
 
 exit 1
 
